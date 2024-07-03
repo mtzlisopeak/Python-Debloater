acfb72ad14ed6d447998b5a218ab4e5fb3d2f62a
Mateus Neves <mateusneves7777@gmail.com>
2024-07-02 20:46:24 -0400

    Corrigi a função (Verificar)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from os import system
+from os import system, chdir
 from subprocess import run
+
+#entra na pasta platform-tools
+chdir("platform-tools")
 
 #função para verificar se algum dispositivo foi reconhecido
 def verificar():
-    verificacao = run("adb shell wm size")
-    if verificacao.returncode == 1:
-        print("Aparelho conectado")
-    else:
-        print("Nenhum dispositivo reconhecido")
+    verificacao = run(["./adb", "shell", "wm", "size"])
+    if verificacao.returncode == 0:
+        system("clear")
+        print("Conectado")
+    elif verificacao.returncode == 1:
+        system("clear")
+        print("Não conectado")
 
 verificar()
