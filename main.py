928fbfe860d35f7233366ae3d7238581934839c0
Mateus Neves <mateusneves7777@gmail.com>
2024-07-03 16:39:55 -0400

    Corrigi algumas funções e coloquei o loop principal dentro da função Verificar

--- main.py
+++ main.py
@@ -8,12 +8,29 @@ chdir("platform-tools")
 
 #função para verificar se algum dispositivo foi reconhecido
 def verificar():
+    global sair
     verificacao = run(["./adb", "shell", "wm", "size"])
 
     if verificacao.returncode == 0:
         limparTerminal()
-        print("Dispositivo Conectado")
-        desinstalarApp()
+        while sair == False:
+            limparTerminal()
+            escolha = int(input("[ 0 ] Desinstalar App\n[ 1 ] Desativar App\n[ 2 ]Sair\n>>>"))
+            
+            if escolha == 0:
+                desinstalarApp()
+
+            elif escolha == 1:
+                break
+
+            elif escolha == 2:
+                limparTerminal()
+                print("Byee")
+                sleep(1)
+                sair = True
+
+            else:
+                print("Opção inválida")
 
     elif verificacao.returncode == 1:
         limparTerminal()
@@ -23,34 +40,34 @@ def limparTerminal():
     system("clear")
 
 def desinstalarApp():
-    app = str(input("Nome do App:"))
-    run(["./adb", "shell", "pm", "uninstall", "--user", "0", app])
+    procurarApp()
+    app = str(input("Desinstalar o App:"))
+    run(["./adb", "shell", "pm", "uninstall", "--user 0", app])
+    sleep(1)
+    while True:
+        limparTerminal()
+        continuar = int(input("[ 0 ] Desinstalar outro App\n[ 1 ] Sair\n>>>"))
+        if continuar == 0:
+            desinstalarApp()
+        
+        elif continuar == 1:
+            break
+
+        else:
+            print("Opção inválida")
+            sleep(1)
 
 def procurarApp():
     pacotes = run(["./adb", "shell", "pm", "list","packages"], text=True, capture_output=True).stdout.splitlines()
 
     limparTerminal()
 
-    apps = [linha.replace("package:", "") for linha in pacotes]
+    apps = [package.replace("package:", "") for package in pacotes]
 
-    filtro = str(input("Digite o nome de um app que deseja:\n>>>"))
+    filtro = str(input("Digite o nome de um app que deseja procurar:\n>>>"))
 
     for app in apps:
         if filtro in app:
             print(app)
 
-while sair == False:
-
-    limparTerminal()
-
-    escolha = int(input("[ 0 ] Desinstalar App\n[ 1 ] Desativar App\n[ 2 ]Sair\n>"))
-    
-    if escolha == 0:
-        pass
-    elif escolha == 1:
-        pass
-    elif escolha == 2:
-        limparTerminal()
-        print("Byee")
-        sleep(1)
-        sair = True
+verificar()
