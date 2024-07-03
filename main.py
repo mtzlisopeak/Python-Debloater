585c0c7cb74093c99f00cded82216b72f028cf05
Mateus Neves <mateusneves7777@gmail.com>
2024-07-02 20:04:48 -0400

    Criei uma função para verificar se o programa reconheceu algum aparelho

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from os import system
 from subprocess import run
 
+#função para verificar se algum dispositivo foi reconhecido
+def verificar():
+    verificacao = run("adb shell wm size")
+    if verificacao.returncode == 1:
+        print("Aparelho conectado")
+    else:
+        print("Nenhum dispositivo reconhecido")
 
+verificar()
