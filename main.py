981f8327989cf68646a7a49448dc655e0ed8ba3b
Mateus Neves <mateusneves7777@gmail.com>
2024-07-03 19:01:14 -0400

    Coreção em algumas funções e simplificação no código
    Corrigi as funções desintalarApp, desativarApp, procurarApp e a função verificar que também mudei seu nome para iniciar

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 from os import system, chdir
 from subprocess import run
 from time import sleep
-sair = False
 
 #entra na pasta platform-tools
 chdir("platform-tools")
 
-#função para verificar se algum dispositivo foi reconhecido
-def verificar():
-    global sair
+#função para iniciar o programa e verifica se tem algum dispositivo conectado no computador
+def iniciar():
+    sair = False
     verificacao = run(["./adb", "shell", "wm", "size"])
 
     if verificacao.returncode == 0:
         limparTerminal()
         while sair == False:
-            limparTerminal()
-            escolha = int(input("[ 0 ] Desinstalar App\n[ 1 ] Desativar App\n[ 2 ] Sair\n>>>"))
             
-            if escolha == 0:
+            escolha = input("[ 1 ] Desinstalar App\n[ 2 ] Desativar App\n[ E ] Sair\n>>>")
+            
+            if escolha == "1":
                 desinstalarApp()
 
-            elif escolha == 1:
+            elif escolha == "2":
                 desativarApp()
 
-            elif escolha == 2:
-                limparTerminal()
+            elif escolha == "E" or escolha == "e":
                 print("Byee")
                 sleep(1)
+                limparTerminal()
                 sair = True
 
             else:
+                limparTerminal()
                 print("Opção inválida")
 
     elif verificacao.returncode == 1:
@@ -40,51 +40,41 @@ def limparTerminal():
     system("clear")
 
 def desinstalarApp():
-    procurarApp()
-    app = str(input("Desinstalar o App:"))
-    run(["./adb", "shell", "pm", "uninstall", "--user 0", app])
-    sleep(1)
     while True:
         limparTerminal()
-        continuar = int(input("[ 0 ] Desinstalar outro App\n[ 1 ] Sair\n>>>"))
-        if continuar == 0:
-            desinstalarApp()
-        
-        elif continuar == 1:
+
+        procurarApp()
+        app = input("Digite o nome do app a ser deinstalado ou aperte [E] para sair:\n>>>")
+        if app == "E" or app == "e":
             break
 
         else:
-            print("Opção inválida")
+            run(["./adb", "shell", "pm", "uninstall", "--user 0", app])
             sleep(1)
+            limparTerminal()
 
 def desativarApp():
-    procurarApp()
-    app = str(input("Desativar o App:"))
-    run(["./adb", "shell", "pm", "disable-user", "--user 0", app])
-    sleep(1)
     while True:
         limparTerminal()
-        continuar = int(input("[ 0 ] Desativar outro app\n[ 1 ] Sair\n>>>"))
-        if continuar == 0:
-            desativarApp()
 
-        elif continuar == 1:
+        procurarApp()
+        app = str(input("Digite o nome do app a ser desativado ou aperte [ E ] para sair:\n>>>")).lower()
+        if app == "E" or app == "e":
             break
-
         else:
-            print("Opção inválida")
+            run(["./adb", "shell", "pm", "disable-user", "--user 0", app])
+            sleep(1)
+            limparTerminal()
 
 def procurarApp():
     pacotes = run(["./adb", "shell", "pm", "list","packages"], text=True, capture_output=True).stdout.splitlines()
-
+    apps = [package.replace("package:", "") for package in pacotes]
     limparTerminal()
 
-    apps = [package.replace("package:", "") for package in pacotes]
-
     filtro = str(input("Digite o nome de um app que deseja procurar:\n>>>"))
+    apps_filtrados = [app for app in apps if filtro in app]
 
-    for app in apps:
-        if filtro in app:
-            print(app)
+    for app in apps_filtrados:
+        print(app)
 
-verificar()
+iniciar()
