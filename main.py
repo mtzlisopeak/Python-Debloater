7c9027739564bbb5a33006af7a026ac889b1a7de
Mateus Neves <mateusneves7777@gmail.com>
2024-07-03 14:45:19 -0400

    Criei as funções limparTerminal, procurarApp, importei sleep da biblioteca time e criei a variavel sair para o loop principal.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from os import system, chdir
 from subprocess import run
+from time import sleep
+sair = False
 
 #entra na pasta platform-tools
 chdir("platform-tools")
@@ -7,17 +9,48 @@ chdir("platform-tools")
 #função para verificar se algum dispositivo foi reconhecido
 def verificar():
     verificacao = run(["./adb", "shell", "wm", "size"])
+
     if verificacao.returncode == 0:
-        system("clear")
+        limparTerminal()
         print("Dispositivo Conectado")
         desinstalarApp()
+
     elif verificacao.returncode == 1:
-        system("clear")
+        limparTerminal()
         print("Nenhum dispositivo conectado")
 
+def limparTerminal():
+    system("clear")
+
 def desinstalarApp():
-    run(["./adb", "shell", "pm", "list", "packages"])
     app = str(input("Nome do App:"))
     run(["./adb", "shell", "pm", "uninstall", "--user", "0", app])
+
+def procurarApp():
+    pacotes = run(["./adb", "shell", "pm", "list","packages"], text=True, capture_output=True).stdout.splitlines()
+
+    limparTerminal()
+
+    apps = [linha.replace("package:", "") for linha in pacotes]
+
+    filtro = str(input("Digite o nome de um app que deseja:\n>>>"))
+
+    for app in apps:
+        if filtro in app:
+            print(app)
+
+while sair == False:
+
+    limparTerminal()
+
+    escolha = int(input("[ 0 ] Desinstalar App\n[ 1 ] Desativar App\n[ 2 ]Sair\n>"))
     
-verificar()
+    if escolha == 0:
+        pass
+    elif escolha == 1:
+        pass
+    elif escolha == 2:
+        limparTerminal()
+        print("Byee")
+        sleep(1)
+        sair = True
